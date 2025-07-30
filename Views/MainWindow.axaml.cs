d049cb75b81e10800f28bfe8660dc16827d35108
hawiliu <hawiliu@gmail.com>
2025-07-30 09:47:48 +0800

    [dev] 修正保持最上層判斷

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -21,22 +21,6 @@ namespace CryptoWidget
                 if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
                     BeginMoveDrag(e);
             };
-
-            // 監聽 KeepOnTop 屬性變化
-            if (mainViewModel.Settings != null)
-            {
-                mainViewModel.Settings.PropertyChanged += OnSettingsPropertyChanged;
-                // 初始化 Topmost 狀態
-                this.Topmost = mainViewModel.Settings.KeepOnTop;
-            }
-        }
-
-        private void OnSettingsPropertyChanged(object? sender, PropertyChangedEventArgs e)
-        {
-            if (e.PropertyName == nameof(Services.SettingsService.KeepOnTop) && sender is Services.SettingsService settings)
-            {
-                this.Topmost = settings.KeepOnTop;
-            }
         }
 
         private void CloseButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
